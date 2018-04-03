d3e15bba1f7fdc2e3c6a9a4310d1243a8223a06c
Rutul Patel <rutulpatel@Rutuls-MacBook-Pro.local>
2018-04-03 11:20:01 -0500

    Add mongodb connection

--- index.js
+++ index.js
@@ -15,6 +15,23 @@
 
 var app = express();
 app.server = http.createServer(app);
+
+// Database config
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/sample_app");
+var db = mongoose.connection;
+
+// print error if there's issue with db connection
+db.on("error", function(err) {
+    console.log("Mongoose error:" , err);
+});
+
+// check if db connection was successful
+db.once("open", function(){
+    console.log("Mongoose connection successful");
+});
+
+//server static route
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(bodyParser.urlencoded({ extended: true}));
